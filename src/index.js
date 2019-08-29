a1b039b36a55a4cb7bc20148c668c7003c1c6c5f
Nikolay Georgiev <ngeorgiev@hilscherdtc.local>
2019-08-29 14:42:10 +0300

    added a router and navigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home";
 import Counters from "./components/counters";
+import Users from "./components/Users";
+import Notfound from "./routes/Notfound";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/counters">Counters</Link>
+        </li>
+        <li>
+          <Link to="/test">404 Page</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/users" component={Users} />
+        <Route path="/counters" component={Counters} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/routes/Notfound.js
@@ -0,0 +1,3 @@
+import React from "react";
+const Notfound = () => <h1>Sorry, Page Not Found</h1>;
+export default Notfound;
